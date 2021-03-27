70d29d606e325f4e0ac7bec896570c8f6012af77
jieun0603 <73928346+jieun0603@users.noreply.github.com>
2021-03-27 19:28:21 +0900

    Create jieun_FirstBadVersion.java

---
+++ Weekly/week_7/jieun_FirstBadVersion.java
@@ -0,0 +1,20 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low = 1;
+        int mid;
+        int high = n;
+        
+        while (low < high) {
+	        mid = low + (high - low) / 2;
+	        if (isBadVersion(mid)) {
+	            high = mid;
+	        } else {
+	            low = mid + 1;
+	        }
+	    }
+        return low;
+    }
+}
