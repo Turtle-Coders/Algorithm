d890d1c29a1b3ab5a1d0825bcec9e79bb1646413
Gibyoung <38740455+keepbang@users.noreply.github.com>
2021-04-05 13:12:09 +0900

    Create JumpGame.js

---
+++ Weekly/week_9/Keepbang/JumpGame.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    if(nums.length == 1) return true;
+    
+    let max = 0;
+    let len = nums.length - 1;
+    for (let i = 0; i < len; i++) {
+        if(nums[i] == 0 && max <= i){
+            return false;
+        }
+        max = Math.max(i + nums[i],max);
+        if(max >= len) return true;
+    }
+    return false;
+};
