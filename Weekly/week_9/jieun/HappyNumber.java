18386094a18ee2bb9c9ce363e597b03db0820ad4
jieun0603 <73928346+jieun0603@users.noreply.github.com>
2021-04-11 20:09:43 +0900

    Update HappyNumber.java

--- Weekly/week_9/jieun/HappyNumber.java
+++ Weekly/week_9/jieun/HappyNumber.java
@@ -24,3 +24,31 @@ class Solution {
         }
     }
 }
+
+------------------------------------------------------------------------------
+
+// 인터넷에서 알고리즘 찾아봄
+
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> cache = new HashSet(); //Set을 통해서 중복된 값들은 자동으로 제거
+        while(!cache.contains(n)){
+            cache.add(n);
+            n = getSum(n);
+            
+            if(n == 1){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public int getSum(int num){
+        int sum = 0;
+        while(num > 0){
+            sum += (num % 10) * (num % 10); // 1의 자릿수
+            num /= 10;                      // 10의 자릿수
+        }
+        return sum;
+    }
+}
