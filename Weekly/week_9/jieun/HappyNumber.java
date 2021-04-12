eb3bcba3ed7c73fba47ef30bb138974d56f28855
jieun0603 <73928346+jieun0603@users.noreply.github.com>
2021-04-12 12:15:31 +0900

    Update HappyNumber.java

--- Weekly/week_9/jieun/HappyNumber.java
+++ Weekly/week_9/jieun/HappyNumber.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean isHappy(int n) {
-        while (n > 1) {
+	Set<Integer> cache = new HashSet(); 
+	while (!cache.contains(n)){
+		while (n > 1) {
 			int total = 0;
 			String[] happyNum = String.valueOf(n).split("");
-			
+
 			for (String StringNum : happyNum) {
 				if (!"".equals(StringNum)) {
 					int num = Integer.parseInt(StringNum);
 					total += num*num;
+					cache.add(total);
 				}
 			}
 			n = total; 
-            
-            if (Integer.MAX_VALUE < total) {
-                break;
-            }
 		}
+	}
         
         if (n != 1){
         	return false;
