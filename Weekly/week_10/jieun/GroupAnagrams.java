39e00d2bc825bc3c1529bb2f659b3c175c6a8a18
jieun0603 <73928346+jieun0603@users.noreply.github.com>
2021-04-19 14:10:53 +0900

    Update GroupAnagrams.java

--- Weekly/week_10/jieun/GroupAnagrams.java
+++ Weekly/week_10/jieun/GroupAnagrams.java
@@ -7,9 +7,15 @@ class Solution {
             Map<String, Object> listMap = new HashMap<>();
             
             for (String word : strs) {
-                String[] sepWord = word.split("");
+                // 내 소스
+                // String[] sepWord = word.split("");
+                // Arrays.sort(sepWord);
+                // String wordSort = String.join("", sepWord);
+                
+                // 다른 사람 소스
+                char[] sepWord = word.toCharArray();
                 Arrays.sort(sepWord);
-                String wordSort = String.join("", sepWord);
+                String wordSort = String.valueOf(sepWord);
 
                 if (listMap.get(wordSort) == null) {
                     List<String> wordList = new ArrayList<>();
