0e55321c061b429c7d21ef81bf37382b6a172d8e
Gibyoung <38740455+keepbang@users.noreply.github.com>
2021-05-06 17:54:43 +0900

    Create SortColors.js

---
+++ Weekly/week_12/Keepbang/SortColors.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    nums.sort() //내장함수 사용
+  //정렬을 직접 구현해서 해보면 괜찮을거같다.
+};
